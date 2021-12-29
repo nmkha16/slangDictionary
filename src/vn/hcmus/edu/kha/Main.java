8a1e058447b4a12f9841e14850693e882568800e
Nguyễn Minh Kha <nmkha19@clc.fitus.edu.vn>
2021-12-29 19:42:46 +0700

    Search by definition/slang + search history
    Complete mentioned feature in tittle for app

--- src/vn/hcmus/edu/kha/Main.java
+++ src/vn/hcmus/edu/kha/Main.java
@@ -1,15 +1,6 @@
 package vn.hcmus.edu.kha;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
 
 public class Main {
     private static MyWindow wnd = null;
@@ -25,94 +16,4 @@ public class Main {
     }
 }
 
-class MyWindow extends JFrame {
-    JLabel label1 = new JLabel("Từ vựng");
-    JLabel label2 = new JLabel("Định nghĩa");
-    public MyWindow() {
-        super("Slang Dictionary");
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                MyWindow.this.setVisible(false);
-                MyWindow.this.dispose();
-            }
-        });
-        setPreferredSize(new Dimension(720, 480));
-        setMinimumSize(new Dimension(720,480));
-        setLayout(new BorderLayout());
-        // set gridbaglayout
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
 
-        // create top panel for list of function
-        // including Label, ComboBox
-        JPanel top_pn = new JPanel();
-        top_pn.setLayout(new FlowLayout());
-        top_pn.add(new JLabel("Chức năng"),gbc);
-        JComboBox functionCB = new JComboBox();
-        top_pn.add(Box.createRigidArea(new Dimension(25, 0)));
-        functionCB.setPreferredSize(new Dimension(300,20));
-        top_pn.add(functionCB,gbc);
-        add(top_pn,BorderLayout.NORTH);
-
-        // create left panel for list of word
-        // left panel
-        JPanel left_pn = new JPanel();
-        left_pn.setLayout(new GridBagLayout());
-        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
-        gbc.gridx++;
-        left_pn.add(label1,gbc);
-        gbc.gridy++;
-        JList functionList = new JList();
-        JScrollPane functionListScroll = new JScrollPane(functionList);
-        functionListScroll.setPreferredSize(new Dimension(200,312));
-        gbc.gridx=0;
-        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
-        gbc.gridx++;
-        left_pn.add(functionListScroll,gbc);
-        gbc.gridy++;
-        JPanel button_panel = new JPanel();
-        button_panel.setLayout(new FlowLayout());
-        JButton delete_btn = new JButton("Delete");
-        JButton add_btn = new JButton("Add");
-        button_panel.add(add_btn);
-        button_panel.add(delete_btn);
-        gbc.gridx=0;
-        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
-        gbc.gridx++;
-        left_pn.add(button_panel,gbc);
-        add(left_pn,BorderLayout.WEST);
-
-        gbc.gridy=0;
-        gbc.gridx=0;
-
-        // create middle pannel for
-        JPanel middle_pn = new JPanel();
-        middle_pn.setLayout(new GridBagLayout());
-        middle_pn.add(label2,gbc);
-        gbc.gridy++;
-        JTextArea jTextArea = new JTextArea();
-        JScrollPane jScrollPane = new JScrollPane(jTextArea);
-        jScrollPane.setPreferredSize(new Dimension(480,350));
-        middle_pn.add(jScrollPane,gbc);
-        add(middle_pn,BorderLayout.CENTER);
-
-        //create bottom pannel
-        JPanel bottom_pn = new JPanel();
-        bottom_pn.setLayout(new FlowLayout());
-        bottom_pn.add(Box.createRigidArea(new Dimension(143, 0)));
-        JTextArea textArea = new JTextArea(2, 25);
-        JScrollPane jsp1 = new JScrollPane(textArea);
-        bottom_pn.add(jsp1);
-        JButton search_btn = new JButton("Search");
-        bottom_pn.add(search_btn);
-        JButton edit_btn = new JButton("Edit");
-        bottom_pn.add(edit_btn);
-        add(bottom_pn,BorderLayout.SOUTH);
-
-        setLocationRelativeTo(null);
-        pack();
-    }
-}
---
+++ src/vn/hcmus/edu/kha/MyWindow.java
@@ -0,0 +1,261 @@
+package vn.hcmus.edu.kha;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class MyWindow extends JFrame {
+    JLabel label1 = new JLabel("Từ vựng");
+    JLabel label2 = new JLabel("Định nghĩa");
+    private JList slangList;
+    private DefaultListModel listModel;
+
+    private JTextArea jTextArea;
+    private JTextField search_input;
+    private Map<String, ArrayList<String>> slang;
+
+    private String[] function = new String[]{"Tìm kiếm","Lịch sử tìm kiếm"};
+    public MyWindow() {
+        super("Slang Dictionary");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                MyWindow.this.setVisible(false);
+                MyWindow.this.dispose();
+            }
+        });
+        // read HashMap
+        slang = readSlang();
+
+
+        // set property of window
+        setPreferredSize(new Dimension(720, 480));
+        setMinimumSize(new Dimension(720,480));
+        setLayout(new BorderLayout());
+        // set gridbaglayout
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+
+        // create top panel for list of function
+        // including Label, ComboBox
+        JPanel top_pn = new JPanel();
+        top_pn.setLayout(new FlowLayout());
+        top_pn.add(new JLabel("Chức năng"),gbc);
+        JComboBox functionCB = new JComboBox(function);
+        functionCB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String event = functionCB.getSelectedItem().toString();
+                if (event.equals("Lịch sử tìm kiếm")){
+                    Scanner input = null;
+                    JTextArea textArea = new JTextArea();
+                    textArea.setEditable(false);
+                    try {
+                        input = new Scanner(new File("history.txt"));
+                        input.useDelimiter("\\A");
+                        if (input.hasNext()) {
+                            textArea.append(input.next());
+                        }
+                    } catch (FileNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                    textArea.setColumns(30);
+                    textArea.setRows(10);
+                    textArea.setLineWrap(true);
+                    textArea.setWrapStyleWord(true);
+                    JPanel panel = new JPanel();  // Create and modify this panel
+                    JOptionPane.showOptionDialog(null,
+                            panel,
+                            "Lịch sử",
+                            JOptionPane.DEFAULT_OPTION,
+                            JOptionPane.PLAIN_MESSAGE,
+                            null,
+                            new Object[] {textArea},
+                            null);
+                }
+                functionCB.setSelectedIndex(0);
+            }
+        });
+        top_pn.add(Box.createRigidArea(new Dimension(25, 0)));
+        functionCB.setPreferredSize(new Dimension(300,20));
+        top_pn.add(functionCB,gbc);
+        add(top_pn,BorderLayout.NORTH);
+
+        // create left panel for list of word
+        // left panel
+        JPanel left_pn = new JPanel();
+        left_pn.setLayout(new GridBagLayout());
+        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
+        gbc.gridx++;
+        left_pn.add(label1,gbc);
+        gbc.gridy++;
+
+        listModel = new DefaultListModel<Set<String>>();
+        listModel.addAll(slang.keySet());
+        slangList = new JList<Set<String>>(listModel);
+        slangList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                try {
+                    ArrayList<String> result = slang.get(slangList.getSelectedValue().toString());
+                    jTextArea.setText(null);
+                    for (String str : result) {
+                        jTextArea.append(str + "\n");
+                    }
+                } catch (Exception ex) {
+                    //ex.printStackTrace();
+                }
+            }
+        });
+        JScrollPane slangListScroll = new JScrollPane(slangList);
+        slangListScroll.setPreferredSize(new Dimension(200,270));
+        gbc.gridx=0;
+        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
+        gbc.gridx++;
+        left_pn.add(slangListScroll,gbc);
+        gbc.gridy++;
+        JPanel button_panel = new JPanel();
+        button_panel.setLayout(new FlowLayout());
+        JButton delete_btn = new JButton("Delete");
+        JButton add_btn = new JButton("Add");
+        button_panel.add(add_btn);
+        button_panel.add(delete_btn);
+        gbc.gridx=0;
+        left_pn.add(Box.createRigidArea(new Dimension(10, 0)),gbc);
+        gbc.gridx++;
+        left_pn.add(button_panel,gbc);
+        add(left_pn,BorderLayout.WEST);
+
+        gbc.gridy=0;
+        gbc.gridx=0;
+
+        // create middle pannel for
+        JPanel middle_pn = new JPanel();
+        middle_pn.setLayout(new GridBagLayout());
+        middle_pn.add(label2,gbc);
+        gbc.gridy++;
+        jTextArea = new JTextArea();
+        JScrollPane jScrollPane = new JScrollPane(jTextArea);
+        jScrollPane.setPreferredSize(new Dimension(480,308));
+        middle_pn.add(jScrollPane,gbc);
+        add(middle_pn,BorderLayout.CENTER);
+
+        //create bottom pannel
+        JPanel bottom_pn = new JPanel();
+        bottom_pn.setPreferredSize(new Dimension(100,70));
+        bottom_pn.setLayout(new FlowLayout());
+
+        JComboBox search_criteria = new JComboBox(new String[]{"Search by slang","Search by definition"});
+        bottom_pn.add(search_criteria);
+        //bottom_pn.add(Box.createRigidArea(new Dimension(50, 0)));
+        search_input = new JTextField(30);
+        bottom_pn.add(search_input);
+        JButton search_btn = new JButton("Search");
+        search_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (search_input.getText().equals("")){
+                    listModel.clear();
+                    listModel.addAll(slang.keySet());
+                    return;
+                }
+                if (search_criteria.getSelectedItem().toString().equals("Search by slang")) {
+                    listModel.clear();
+                    Pattern pattern = Pattern.compile(Pattern.quote(search_input.getText()), Pattern.CASE_INSENSITIVE);
+                    for (String key : slang.keySet()) {
+                        Matcher matcher = pattern.matcher(key);
+                        if (matcher.find()) {
+                            listModel.addElement(key);
+                        }
+                    }
+                    // write to history file
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+                    Date date = new Date();
+                    try {
+                        PrintWriter writer = new PrintWriter(new FileOutputStream(new File("history.txt"),
+                                true));
+                        writer.append(formatter.format(date)+" : " + search_input.getText()+"\n");
+                        writer.close();
+                    } catch (FileNotFoundException ex) {
+                        ex.printStackTrace();
+                        //done
+                    }
+                }
+                else{
+                    listModel.clear();
+                    Pattern pattern = Pattern.compile(Pattern.quote(search_input.getText()), Pattern.CASE_INSENSITIVE);
+                    for (Map.Entry<String, ArrayList<String>> entry : slang.entrySet()) {
+                        String key = entry.getKey();
+                        ArrayList<String> value = entry.getValue();
+                        for (String str : value) {
+                            Matcher matcher = pattern.matcher(str);
+                            if (matcher.find()) {
+                                listModel.addElement(key);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        bottom_pn.add(search_btn);
+        JButton edit_btn = new JButton("Edit");
+        bottom_pn.add(edit_btn);
+        add(bottom_pn,BorderLayout.SOUTH);
+
+
+        setLocationRelativeTo(null);
+        pack();
+    }
+
+    /**
+     * Return a hashmap of slang and its definitions
+     * Also set hashmap of definition and its slang
+     * @return Map<String, ArrayList<String>>
+     */
+    private Map<String, ArrayList<String>> readSlang(){
+        Map<String, ArrayList<String>> result = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("slang.txt"))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                // skip header
+                if (line.equals("Slag`Meaning")){continue;}
+                if (line.contains("`")) {
+                    ArrayList<String> def = new ArrayList<>();
+                    String[] token = line.split("(`)");
+                    // first index is slang
+                    // second index is definition
+                    if (token[1].contains("|")) {
+                        String[] definitions = token[1].split("\\| ");
+
+                        for (String definition : definitions) {
+                            def.add(definition);
+                        }
+
+                        result.put(token[0], def);
+                    } else {
+                        def.add(token[1]);
+                        result.put(token[0], def);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
